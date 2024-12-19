1361a60ef805d277a80fee2fa6d9f5bffdef3fac
Duncan Moiyo <duncan.moiyo@gmail.com>
2024-12-19 21:32:50 +0300

    add docker file

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,7 +1,9 @@
 <?php
+
 declare(strict_types=1);
 
 use App\Middlewares\ResponseMiddleware;
+use Selective\BasePath\BasePathMiddleware;
 use Slim\Factory\AppFactory;
 
 require_once __DIR__ . "/../vendor/autoload.php";
@@ -10,9 +12,11 @@ $dotEnv->load();
 $app = AppFactory::create();
 $app->addRoutingMiddleware();
 $app->addBodyParsingMiddleware();
-$error_middleware=$app->addErrorMiddleware(true, true, true);
+$app->add(new BasePathMiddleware($app));
+$error_middleware = $app->addErrorMiddleware(true, true, true);
 $error_handler = $error_middleware->getDefaultErrorHandler();
 $error_handler->forceContentType('application/json');
+
 $app->add(ResponseMiddleware::class);
 require __DIR__ . "/../config/routes.php";
 
